b5b2766d58165aa78da142dc474b6f5f42bc323e
Debasree <debasreerocz888@gmail.com>
2019-07-02 13:09:14 +0530

    dashboard template upload

---
+++ dashboard template/template_footer.php
@@ -0,0 +1,24 @@
+<!-- footer content -->
+        <footer>
+            <div class="pull-right">
+                Library Management System
+            </div>
+            <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+    </div>
+</div>
+
+<!-- jQuery -->
+<script src="js/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="js/bootstrap.min.js"></script>
+<!-- FastClick -->
+<script src="js/fastclick.js"></script>
+<!-- NProgress -->
+<script src="js/nprogress.js"></script>
+
+<!-- Custom Theme Scripts -->
+<script src="js/custom.min.js"></script>
+</body>
+</html>
